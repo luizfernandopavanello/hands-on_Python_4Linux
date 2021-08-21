e847969a376e7ce3e01eaf7d9d8177dd7c4f4741
Luiz Pavanello <luizpavanello@gmail.com>
2021-08-21 15:16:55 -0300

    Start the first project with 4Linux

---
+++ basics.py
@@ -0,0 +1,14 @@
+print('Hello World')
+
+# input('Username: ')
+
+if 2 + 2 == 4:
+  print('Answer is 4')
+elif 2 + 2 == 5:
+  print('Noooo')
+else:
+  print('Math is  hard')
+
+
+for x in range(1, 5):
+  print('Number: ', x)
