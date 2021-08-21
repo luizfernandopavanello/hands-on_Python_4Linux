5f2c78f740670f90f1fb00dde73a4d5d7b5696b3
Luiz Pavanello <luizpavanello@gmail.com>
2021-08-21 17:32:58 -0300

    Done with numbers, conditions, tuple...

--- imc/imc.py
+++ imc/imc.py
@@ -1,4 +1,35 @@
+from typing import Tuple
 
+escala_imc = {
+  'amp': 'Muito abaixo do peso',
+  'ap': 'Abaixo do peso',
+  'pi': 'Peso ideal',
+  'ac': 'Acima do peso',
+  'o1': 'Obesidade 1',
+  'o2': 'Obesidade 2',
+  'o3': 'Obesidade 3'
+}
+
+def calcular_imc(peso_kg: float, altura_m: float)-> Tuple[float, str] :
+  imc = peso_kg / altura_m ** 2
+
+  if imc < 17:
+    situacao = 'amp'
+  elif imc <= 18.5:
+    situacao = 'ap'
+  elif imc <= 25:
+    situacao = 'pi'
+  elif imc <= 30:
+    situacao = 'ac'
+  elif imc <= 35:
+    situacao = 'o1'
+  elif imc <= 40:
+    situacao = 'o2'
+  else:
+    situacao = 'o3'
+
+  saida = (imc, situacao) 
+  return saida
 
 def main() :
   try:
@@ -10,30 +41,13 @@ def main() :
     print('Valor inválido')
     return
 
-  imc = peso / altura ** 2
-  
-  if imc < 17:
-    situacao = 'Muito abaixo do peso'
-  
-  elif imc <= 18.5:
-    situacao = 'Abaixo do peso'
+  resultado = calcular_imc(peso, altura)
+  (imc, referencia) = resultado
 
-  elif imc <= 25:
-    situacao = 'Peso ideal'
-  
-  elif imc <= 30:
-    situacao = 'Acima do peso'
-  
-  elif imc <= 35:
-    situacao = 'Obesidade 1'
-  
-  elif imc <= 40:
-    situacao = 'Obesidade 2'
-
-  else:
-    situacao = 'Obesidade 3'
+  situacao = escala_imc[referencia]
 
   print('O seu imc é igual a:', imc)
   print('A sua situacao é:', situacao)
 
-main()
+if __name__ == "__main__":
+  main()
